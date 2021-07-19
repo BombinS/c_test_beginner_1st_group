74cc337c16eb762ba96ed5076a1fdae9ebfe0465
Sergey Bombin <sergey.bombin.1977@gmail.com>
2021-07-19 20:30:21 +0300

    первая часть разбора примера андрея

--- tst_1.c
+++ tst_1.c
@@ -3,19 +3,65 @@
 #include <setjmp.h>
 #include "cmocka\include\cmocka.h"
 
+int function_H (int a, int b, int c, int d, int H);
+int result;
 
 static void test_1(void **state) {
+    result = function_H(9999, 9999, 9999, 9999, 0);
+    assert_int_equal(result ,1);
+}
 
-    // вот здесь (то есть ниже) вы можете делать все что захотите (в пределах синтаксиса языка Си)
+static void test_2(void **state) {
+    result = function_H(123, 123, 123, 123, 0);
+    assert_int_equal(result ,0);
+}
 
+static void test_3(void **state) {
+    result = function_H(123, 9999, 9999, 9999, 0);
+    assert_int_equal(result ,1);
+}
 
+static void test_4(void **state) {
+    result = function_H(9999, 123, 9999, 9999, 0);
+    assert_int_equal(result ,1);
+}
 
-    // все, заканчиваем... ниже ничего писать не надо
+static void test_5(void **state) {
+    result = function_H(9999, 9999, 123, 9999, 0);
+    assert_int_equal(result ,1);
+}
+
+static void test_6(void **state) {
+    result = function_H(9999, 9999, 9999, 123, 0);
+    assert_int_equal(result ,1);
+}
+
+static void test_7(void **state) {
+    result = function_H(123, 123, 9999, 9999, 0);
+    assert_int_equal(result ,0);
+}
+
+static void test_8(void **state) {
+    result = function_H(123, 9999, 123, 9999, 0);
+    assert_int_equal(result ,0);
+}
+
+static void test_9(void **state) {
+    result = function_H(123, 123, 123, 9999, 0);
+    assert_int_equal(result ,0);
 }
 
 int main(void) {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test( test_1 ),
+        cmocka_unit_test( test_2 ),
+        cmocka_unit_test( test_3 ),
+        cmocka_unit_test( test_4 ),
+        cmocka_unit_test( test_5 ),
+        cmocka_unit_test( test_6 ),                                
+        cmocka_unit_test( test_7 ),                                
+        cmocka_unit_test( test_8 ),                                
+        cmocka_unit_test( test_9 ),
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
