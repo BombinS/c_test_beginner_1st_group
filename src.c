de68af0cfb4ddd319fec31d73560da28d9dcfe09
Sergey Bombin <sergey.bombin.1977@gmail.com>
2021-07-22 20:53:29 +0300

    циклы... начало

---
+++ src.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+ 
+    int sveta;
+    for (sveta = 0; sveta < 10; sveta++) 
+    {
+        int dima;
+        for (dima = 0; dima < 10; dima++)
+        {
+            printf("x");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
